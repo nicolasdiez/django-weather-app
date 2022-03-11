b4750d9ec953edf34dc3f0cab49ea1eaebd09e81
nicolasdiez <nicolas439@gmail.com>
2022-03-11 17:19:55 +0100

    Update settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config #So in order to use SECRET_KEY parametrization
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t1p31_5hd04#d+4=^-ol@bxl@!6z%tdnzt-ac+za*7f48=n1)z'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
