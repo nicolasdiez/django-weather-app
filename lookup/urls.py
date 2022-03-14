8ba79d92668e401785f37f0e443eead388363082
Nico Diez <nicolas439@gmail.com>
2022-03-14 16:47:38 +0100

    Added search by ZIP and Full report button

--- lookup/urls.py
+++ lookup/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #el path está vacio xq es la home ppal, de ahí colgaran el resto de webpages, ej: /about.html
+    
+    #Home.html - el path está vacio xq es la home ppal, de ahí colgaran el resto de webpages, ej: /about.html
     path('', views.home, name="home"),
 
+    #About.html
     path('about.html', views.about, name="about"),
 
 ]
--- lookup/views.py
+++ lookup/views.py
@@ -5,16 +5,86 @@ def home(request):
     import json
     import requests #needs to be installed first -> $ pip install requests
 
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=6EF830AC-9603-4B0E-954B-18C23C271DCD")
+    if request.method == "POST":
 
-    try: 
-        #load up into a JSON the content of api_request
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        zipcode = request.POST['zipcode']
+        
+        api_url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zipcode + "&distance=5&API_KEY=6EF830AC-9603-4B0E-954B-18C23C271DCD"
+        api_request = requests.get(api_url)
 
-    #make variable 'api' available into the context of the home.html file
-    return render(request, 'home.html', {'api':api}) 
+        try: 
+            #load up into a JSON the content of api_request
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        #In the .HTML file would be --> {% if api.0.Category.Name == "Good" %}
+        if api[0]['Category']['Name'] == "Good":
+            category_description = "[0 to 50] --> Air quality is satisfactory, and air pollution poses little or no risk."
+            category_color = "good"
+            reporting_area = api[0]['ReportingArea']
+            state_code = api[0]['StateCode']
+            full_info_url = "https://www.airnow.gov/?reportingArea=" + reporting_area + "&stateCode=" + state_code
+        elif api[0]['Category']['Name'] == "Moderate":
+            category_description = "[51 to 100] --> Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
+            category_color = "moderate"
+        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "[101 to 150] --> Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+            category_color = "unhealthyforsensitivegroups"
+        elif api[0]['Category']['Name'] == "Unhealthy":
+            category_description = "[151 to 200] --> Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
+            category_color = "unhealthy"
+        elif api[0]['Category']['Name'] == "Very Unhealthy":
+            category_description = "[201 to 300] --> Health alert: The risk of health effects is increased for everyone."
+            category_color = "veryunhealthy"
+        elif api[0]['Category']['Name'] == "Hazardous":
+            category_description = "[301 and higher] --> Health warning of emergency conditions: everyone is more likely to be affected."
+            category_color = "hazardous"
+
+        #make variables 'api','category_description' and 'category_color' available into the context of the home.html file
+        return render(request, 'home.html', {
+            'api': api,
+            'category_description': category_description,
+            'category_color': category_color,
+            'full_info_url': full_info_url
+            }) 
+ 
+    else:
+        api_url = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=6EF830AC-9603-4B0E-954B-18C23C271DCD"
+        api_request = requests.get(api_url)
+
+        try: 
+            #load up into a JSON the content of api_request
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        #In the .HTML file would be --> {% if api.0.Category.Name == "Good" %}
+        if api[0]['Category']['Name'] == "Good":
+            category_description = "[0 to 50] --> Air quality is satisfactory, and air pollution poses little or no risk."
+            category_color = "good"
+        elif api[0]['Category']['Name'] == "Moderate":
+            category_description = "[51 to 100] --> Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution."
+            category_color = "moderate"
+        elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "[101 to 150] --> Members of sensitive groups may experience health effects. The general public is less likely to be affected."
+            category_color = "unhealthyforsensitivegroups"
+        elif api[0]['Category']['Name'] == "Unhealthy":
+            category_description = "[151 to 200] --> Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects."
+            category_color = "unhealthy"
+        elif api[0]['Category']['Name'] == "Very Unhealthy":
+            category_description = "[201 to 300] --> Health alert: The risk of health effects is increased for everyone."
+            category_color = "veryunhealthy"
+        elif api[0]['Category']['Name'] == "Hazardous":
+            category_description = "[301 and higher] --> Health warning of emergency conditions: everyone is more likely to be affected."
+            category_color = "hazardous"
+
+        #make variables 'api','category_description' and 'category_color' available into the context of the home.html file
+        return render(request, 'home.html', {
+            'api': api,
+            'category_description': category_description,
+            'category_color': category_color
+            }) 
 
 def about(request):
     return render(request, 'about.html', {})
