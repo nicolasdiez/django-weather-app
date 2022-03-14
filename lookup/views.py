62e81aeae7914794e8c25d541eba4574662d8b13
Nico Diez <nicolas439@gmail.com>
2022-03-14 10:00:11 +0100

    Integrate first request Air Now API

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests #needs to be installed first -> $ pip install requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=6EF830AC-9603-4B0E-954B-18C23C271DCD")
+
+    try: 
+        #load up into a JSON the content of api_request
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    #make variable 'api' available into the context of the home.html file
+    return render(request, 'home.html', {'api':api}) 
 
 def about(request):
     return render(request, 'about.html', {})
